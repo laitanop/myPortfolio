011d83a876bcfb93eb2df7ea3b2341f1ec434b5c
laitanop <laitanop@yahoo.com>
2025-08-14 19:28:35 -0600

    fix server

--- server.mjs
+++ server.mjs
@@ -1,11 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use("/", express.static("public"));
+// Create __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use("/blog-template", express.static("blog-template/public"));
+// Serve static files from the "public" directory
+app.use("/", express.static(path.join(__dirname, "public")));
 
+// Serve static files from the "blog-template/public" directory
+app.use(
+  "/blog-template",
+  express.static(path.join(__dirname, "blog-template/public"))
+);
+
+// Start the server
 app.listen(port, () => {
   console.log(`Example app listening on port: http://localhost:${port}`);
 });
